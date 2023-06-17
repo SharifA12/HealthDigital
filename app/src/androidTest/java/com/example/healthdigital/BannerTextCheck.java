7a33b291f6b60500dea7415eb3bcea5448ef8be8
SharifA12 <72459322+SharifA12@users.noreply.github.com>
2023-06-17 14:17:51 -0700

    test

---
+++ app/src/androidTest/java/com/example/healthdigital/BannerTextCheck.java
@@ -0,0 +1,43 @@
+package com.example.healthdigital;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class BannerTextCheck {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void bannerTextCheck() {
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.textView), withText("Nurses are the heart of healthcare!"),
+                        withParent(withParent(withId(R.id.frame_layout))),
+                        isDisplayed()));
+        textView.check(matches(withText("Nurses are the heart of healthcare!")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.textView), withText("Nurses are the heart of healthcare!"),
+                        withParent(withParent(withId(R.id.frame_layout))),
+                        isDisplayed()));
+        textView2.check(matches(withText("Nurses are the heart of healthcare!")));
+    }
+}
